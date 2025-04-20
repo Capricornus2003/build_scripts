7be0b4492af218c86a1b7a87e71b7db25730e148
Murtaza Hussain <160129300+MurtazaKolachi@users.noreply.github.com>
2025-04-20 13:58:55 +0500

    RisingOS Revived

--- RisingOS Revived.sh
+++ RisingOS Revived.sh
@@ -1,11 +1,11 @@
 rm -rf .repo/local_manifests ; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi ; \
-repo init --depth=1 --no-repo-verify -u https://github.com/RisingOS-Revived/android -b fifteen -g default,-mips,-darwin,-notdefault && \
-/opt/crave/resync.sh && \
+repo init --depth=1 --no-repo-verify -u https://github.com/RisingOS-Revived/android -b qpr2 -g default,-mips,-darwin,-notdefault && \
 git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo -b rising device/xiaomi/apollo && \
 git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
 git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
 git clone https://github.com/MurtazaKolachi/hardware_xiaomi -b fifteen-old hardware/xiaomi && \
+/opt/crave/resync.sh && \
 export BUILD_USERNAME=Murtaza ; \
 export BUILD_HOSTNAME=crave ; \
 export TZ=Asia/islamabad ; \
