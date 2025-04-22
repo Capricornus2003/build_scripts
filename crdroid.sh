fdc61e916c8e743e30aa610b18ce88559339f370
Capricornus2003 <37134725+Capricornus2003@users.noreply.github.com>
2025-04-22 17:11:53 +0800

    Update crdroid.sh

--- crdroid.sh
+++ crdroid.sh
@@ -2,25 +2,23 @@ rm -rf .repo/local_manifests ; \
 rm -rf {device,vendor,kernel,hardware}/xiaomi ; \
 repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android -b 15.0 -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
-git clone https://github.com/MurtazaKolachi/android_device_xiaomi_apollo_fix -b crdroid device/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/android_vendor_xiaomi_apollo -b main vendor/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/kernel_xiaomi_apollo -b main kernel/xiaomi/apollo && \
-git clone https://github.com/MurtazaKolachi/hardware_xiaomi -b fifteen hardware/xiaomi && \
-rm -rf  hardware/qcom-caf/common; \
-git clone https://github.com/MurtazaKolachi/android_hardware_qcom-caf_common -b lineage-22.2 hardware/qcom-caf/common && \
+git clone https://github.com/crdroidandroid/android_vendor_xiaomi_jason -b 15.0 vendor/xiaomi/jason && \
+git clone https://github.com/crdroidandroid/android_kernel_xiaomi_jason -b 15.0 kernel/xiaomi/jason && \
+git clone https://github.com/crdroidandroid/android_device_xiaomi_jason -b 15.0 device/xiaomi/jason && \
+git clone https://github.com/crdroidandroid/android_hardware_xiaomi -b 15.0 hardware/xiaomi && \
 export BUILD_USERNAME=Murtaza ; \
 export BUILD_HOSTNAME=crave ; \
-export TZ=Asia/islamabad ; \
+export TZ=Asia/Taipei ; \
 source build/envsetup.sh && \
-breakfast apollo user && \
+breakfast jason userdebug && \
 #Build GMS
 mka bacon
 #To get the build:
 mv out/target/product/apollo/crDroid*.zip .
-curl -T "crDroid*.zip" -u :6604f309-17e3-4a0d-bde2-87ae3a58eff0 https://pixeldrain.com/api/file/
+curl -T "crDroid*.zip" -u :9db1fe94-0290-4a17-b317-b79c3957ac25 https://pixeldrain.com/api/file/
 #Build Vanilla
 export WITH_GMS=true
 mka bacon
 #To get the build:
 mv out/target/product/apollo/crDroid*.zip .
-curl -T "crDroid*.zip" -u :6604f309-17e3-4a0d-bde2-87ae3a58eff0 https://pixeldrain.com/api/file/
+curl -T "crDroid*.zip" -u :59770441-c926-43c0-b338-c8239eea4fe2 https://pixeldrain.com/api/file/
